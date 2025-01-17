b51bba2378826aa7678afa1223b21125fe5ab109
user1 <user1@DESKTOP-JDCC25S>
2025-01-17 17:46:28 +0900

    [ADD] JDBCRun PreparedStatement로 변경하기
    기존에 Statement 사용했던 코딩과 비교해보기

--- src/com/milktea/jdbc/day01/basic/JDBCRun.java
+++ src/com/milktea/jdbc/day01/basic/JDBCRun.java
@@ -2,6 +2,7 @@ package com.milktea.jdbc.day01.basic;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -40,10 +41,6 @@ public class JDBCRun {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
 	public void dqlJDBC(String[] args) {
 		/*
 		 * 1. 드라이버 등록
@@ -80,7 +77,95 @@ public class JDBCRun {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} 
-		
-	
 	}	
+	// PreparedStatement
+	public void preDmlJdbc(String[] args) {
+		// DML JDBC
+		String query = "INSERT INTO STUDENT_TBL VALUES(?,?,?,?,?,?,?,?,?,DEFAULT)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			// 1. 드라이버 등록
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 2. DBMS 연결
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			// 3. Statement 생성, PreparedStatement 생성
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, "user03");
+			pstmt.setString(2, "pass03");
+			pstmt.setString(3, "삼올라");
+			pstmt.setString(4, "F");
+			pstmt.setInt(5, 33);
+			pstmt.setString(6, "user03@ola.com");
+			pstmt.setString(7, "01099887766");
+			pstmt.setString(8, "경기도 광주시");
+			pstmt.setString(9, "코딩,수영");
+			// 4. SQL전송 및 5. 결과받기
+			int result = pstmt.executeUpdate();
+			if(result > 0) {
+				System.out.println("데이터가 저장되었습니다.");
+			}else {
+				System.out.println("데이터 저장이 완료되지 않았습니다.");
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 6. 자원해제
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+	public void preDqlJDBC() {
+		String query = "SELECT * FROM STUDENT_TBL";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		try {
+			// 1. 드라이버 등록
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 2. DBMS 연결
+			conn = DriverManager.getConnection(
+						"jdbc:oracle:thin:@localhost:1521:XE"
+						, "STUDENT"
+						, "STUDENT");
+			// 3. Statement 생성, PreparedStatement 생성
+			pstmt = conn.prepareStatement(query);
+			// pstmt.setString(1, memberId); // 위치홀더 있으면 생략 불가능
+			
+			// 4. SQL 전송 및 5. 결과받기
+			//rset = stmt.executeQuery(query);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				System.out.println("ID : " + rset.getString("STUDENT_ID"));
+				System.out.println("NAME : " + rset.getString("STUDENT_NAME"));
+				System.out.println("AGE : " + rset.getInt("AGE"));
+				System.out.println("DATE : " + rset.getDate("ENROLL_DATE"));
+			}
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 6. 자원해제
+			try {
+				rset.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
